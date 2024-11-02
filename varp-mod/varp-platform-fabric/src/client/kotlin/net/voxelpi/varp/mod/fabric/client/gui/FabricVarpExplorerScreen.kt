590f60e5bd1eb68a9b659b129bce1b3ad99e75c7
Peter Smek <smek.peter@gmail.com>
2024-11-02 22:50:49 +0100

    Add tree view to explorer gui

--- varp-mod/varp-platform-fabric/src/client/kotlin/net/voxelpi/varp/mod/fabric/client/gui/FabricVarpExplorerScreen.kt
+++ varp-mod/varp-platform-fabric/src/client/kotlin/net/voxelpi/varp/mod/fabric/client/gui/FabricVarpExplorerScreen.kt
@@ -3,10 +3,14 @@ package net.voxelpi.varp.mod.fabric.client.gui
 import io.wispforest.owo.ui.base.BaseOwoScreen
 import io.wispforest.owo.ui.container.Containers
 import io.wispforest.owo.ui.container.FlowLayout
+import io.wispforest.owo.ui.core.Insets
 import io.wispforest.owo.ui.core.OwoUIAdapter
+import io.wispforest.owo.ui.core.Size
 import io.wispforest.owo.ui.core.Sizing
 import io.wispforest.owo.ui.core.Surface
 import net.voxelpi.varp.mod.fabric.client.gui.component.ExplorerMenuBar
+import net.voxelpi.varp.mod.fabric.client.gui.component.ExplorerTreeView
+import net.voxelpi.varp.warp.NodeParent
 import net.voxelpi.varp.warp.Tree
 import net.voxelpi.varp.warp.path.NodeParentPath
 
@@ -18,7 +22,39 @@ class FabricVarpExplorerScreen(
     var viewPath: NodeParentPath = viewPath
         private set
 
-    private val menuBar: ExplorerMenuBar = ExplorerMenuBar(viewPath, Sizing.fill(100), Sizing.content())
+    private val menuBar: ExplorerMenuBar = ExplorerMenuBar(viewPath, Sizing.fill(100), Sizing.content()).apply {
+        createWarpAction = {
+            uiAdapter.toggleInspector()
+        }
+    }
+
+    private val treeView = ExplorerTreeView(tree, Sizing.content(), Sizing.content(), this::changeViewContainer)
+
+    private val body = object : FlowLayout(Sizing.fill(100), Sizing.fill(100), Algorithm.HORIZONTAL) {
+
+        init {
+            child(
+                Containers.verticalScroll(Sizing.fixed(120), Sizing.fill(100), treeView).apply {
+                    surface(Surface.DARK_PANEL)
+                    padding(Insets.of(4, 4, 8, 4))
+                    margins(Insets.of(0, 4, 4, 4))
+                }
+            )
+        }
+
+        override fun layout(space: Size?) {
+            sizing(Sizing.fill(), Sizing.fill())
+            super.layout(space)
+
+            val maxHeight = fullSize().height() - menuBar.fullSize().height
+            sizing(Sizing.fill(), Sizing.fixed(maxHeight))
+            for (child in children()) {
+                child.verticalSizing(Sizing.fixed(maxHeight))
+            }
+
+            super.layout(space)
+        }
+    }
 
     override fun createAdapter(): OwoUIAdapter<FlowLayout> {
         return OwoUIAdapter.create(this, Containers::verticalFlow)
@@ -28,6 +64,7 @@ class FabricVarpExplorerScreen(
         rootComponent.surface(Surface.VANILLA_TRANSLUCENT)
 
         rootComponent.child(menuBar)
+        rootComponent.child(body)
 
         changeViewPath(viewPath)
     }
@@ -37,8 +74,12 @@ class FabricVarpExplorerScreen(
     }
 
     fun changeViewPath(path: NodeParentPath) {
-        this.viewPath = path
+        changeViewContainer(tree.resolve(path) ?: tree.root)
+    }
 
-        menuBar.path = path
+    fun changeViewContainer(parent: NodeParent) {
+        this.viewPath = parent.path
+
+        menuBar.path = parent.path
     }
 }
---
+++ varp-mod/varp-platform-fabric/src/client/kotlin/net/voxelpi/varp/mod/fabric/client/gui/component/ExplorerTreeView.kt
@@ -0,0 +1,165 @@
+package net.voxelpi.varp.mod.fabric.client.gui.component
+
+import io.wispforest.owo.ui.component.Components
+import io.wispforest.owo.ui.component.LabelComponent
+import io.wispforest.owo.ui.container.Containers
+import io.wispforest.owo.ui.container.FlowLayout
+import io.wispforest.owo.ui.core.Component
+import io.wispforest.owo.ui.core.Component.FocusSource
+import io.wispforest.owo.ui.core.CursorStyle
+import io.wispforest.owo.ui.core.Insets
+import io.wispforest.owo.ui.core.OwoUIDrawContext
+import io.wispforest.owo.ui.core.ParentComponent
+import io.wispforest.owo.ui.core.Sizing
+import io.wispforest.owo.ui.core.Surface
+import io.wispforest.owo.ui.util.Delta
+import io.wispforest.owo.ui.util.UISounds
+import net.minecraft.text.Text
+import net.minecraft.util.math.RotationAxis
+import net.voxelpi.varp.mod.fabric.client.util.clientNative
+import net.voxelpi.varp.warp.Folder
+import net.voxelpi.varp.warp.NodeParent
+import net.voxelpi.varp.warp.Tree
+import org.lwjgl.glfw.GLFW
+import kotlin.collections.sortedBy
+
+class ExplorerTreeView(
+    val tree: Tree,
+    horizontalSizing: Sizing,
+    verticalSizing: Sizing,
+    val selectionCallback: (node: NodeParent) -> Unit,
+) : FlowLayout(horizontalSizing, verticalSizing, Algorithm.VERTICAL) {
+
+    init {
+        updateContent()
+    }
+
+    fun updateContent() {
+        clearChildren()
+        child(Entry(tree.root, selectionCallback))
+    }
+
+    class Entry(
+        private val node: NodeParent,
+        private val selectionCallback: (node: NodeParent) -> Unit,
+    ) : FlowLayout(Sizing.content(), Sizing.content(), Algorithm.VERTICAL) {
+
+        private val spinnyBoiComponent: SpinnyBoiComponent
+        private val headerLayout: FlowLayout
+        private val bodyLayout: FlowLayout
+
+        var expanded: Boolean = true
+
+        private var content: MutableList<Component> = mutableListOf()
+
+        init {
+            spinnyBoiComponent = SpinnyBoiComponent()
+            spinnyBoiComponent.targetRotation = if (expanded) 90f else 0f
+            spinnyBoiComponent.rotation = spinnyBoiComponent.targetRotation
+
+            headerLayout = Containers.horizontalFlow(Sizing.content(), Sizing.content())
+            headerLayout.padding(Insets.of(3, 3, 0, 0))
+            headerLayout.child(
+                spinnyBoiComponent
+            )
+            headerLayout.child(
+                Components.label(node.name.clientNative())
+            )
+            super.child(headerLayout)
+
+            bodyLayout = Containers.verticalFlow(Sizing.content(), Sizing.content())
+            bodyLayout.padding(Insets.left(8))
+            bodyLayout.surface(SURFACE)
+            super.child(bodyLayout)
+
+            for (folder in node.childFolders().sortedBy(Folder::id)) {
+                content.add(Entry(folder, selectionCallback))
+            }
+            if (expanded) {
+                bodyLayout.children(content)
+            }
+        }
+
+        fun toggleExpansion() {
+            if (expanded) {
+                bodyLayout.clearChildren()
+                spinnyBoiComponent.targetRotation = 0f
+            } else {
+                bodyLayout.children(content)
+                spinnyBoiComponent.targetRotation = 90f
+            }
+
+            expanded = !expanded
+        }
+
+        override fun canFocus(source: FocusSource): Boolean {
+            return source == FocusSource.KEYBOARD_CYCLE
+        }
+
+        override fun onKeyPress(keyCode: Int, scanCode: Int, modifiers: Int): Boolean {
+            if (keyCode == GLFW.GLFW_KEY_SPACE || keyCode == GLFW.GLFW_KEY_ENTER || keyCode == GLFW.GLFW_KEY_KP_ENTER) {
+                toggleExpansion()
+                super.onKeyPress(keyCode, scanCode, modifiers)
+                return true
+            }
+
+            return super.onKeyPress(keyCode, scanCode, modifiers)
+        }
+
+        override fun onMouseDown(mouseX: Double, mouseY: Double, button: Int): Boolean {
+            val superResult = super.onMouseDown(mouseX, mouseY, button)
+
+            return if (mouseY <= this.headerLayout.fullSize().height && !superResult) {
+                if (mouseX <= this.spinnyBoiComponent.fullSize().width) {
+                    toggleExpansion()
+                    UISounds.playInteractionSound()
+                } else {
+                    selectionCallback.invoke(node)
+                    UISounds.playButtonSound()
+                }
+                true
+            } else {
+                superResult
+            }
+        }
+
+        class SpinnyBoiComponent : LabelComponent(Text.literal(">")) {
+            var rotation: Float = 90f
+            var targetRotation: Float = 90f
+
+            init {
+                margins(Insets.of(0, 0, 4, 4))
+                cursorStyle(CursorStyle.HAND)
+            }
+
+            override fun update(delta: Float, mouseX: Int, mouseY: Int) {
+                super.update(delta, mouseX, mouseY)
+                rotation += Delta.compute(rotation, targetRotation, delta * 0.65f)
+            }
+
+            override fun draw(context: OwoUIDrawContext, mouseX: Int, mouseY: Int, partialTicks: Float, delta: Float) {
+                val matrices = context.matrices
+
+                matrices.push()
+                matrices.translate(x + width / 2f - 1, y + height / 2f - 1, 0f)
+                matrices.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(rotation))
+                matrices.translate(-(x + width / 2f - 1), -(y + height / 2f - 1), 0f)
+
+                super.draw(context, mouseX, mouseY, partialTicks, delta)
+                matrices.pop()
+            }
+        }
+
+        companion object {
+            private val SURFACE = Surface { context: OwoUIDrawContext, component: ParentComponent ->
+                context.fill(
+                    component.x() + 4,
+                    component.y(),
+                    component.x() + 5,
+                    component.y() + component.height(),
+                    0x77FFFFFF
+                )
+            }
+        }
+    }
+}
