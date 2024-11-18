13516fd850ef5b75be33aa8618a49d9e16d46994
Peter Smek <smek.peter@gmail.com>
2024-11-18 01:32:15 +0100

    Add loader default config

--- varp-loader/src/main/kotlin/net/voxelpi/varp/loader/model/RepositoryDefinition.kt
+++ varp-loader/src/main/kotlin/net/voxelpi/varp/loader/model/RepositoryDefinition.kt
@@ -3,6 +3,7 @@ package net.voxelpi.varp.loader.model
 import net.voxelpi.varp.warp.repository.RepositoryConfig
 import net.voxelpi.varp.warp.repository.RepositoryType
 
+@JvmRecord
 internal data class RepositoryDefinition(
     val id: String,
     val type: RepositoryType<*, *>,
