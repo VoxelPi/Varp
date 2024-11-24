e39909bc5e37f60d67559d08f44368ac0a03dc2e
Peter Smek <smek.peter@gmail.com>
2024-11-24 20:09:26 +0100

    Update tags command of cli

--- varp-cli/src/main/kotlin/net/voxelpi/varp/cli/command/commands/TagsCommand.kt
+++ varp-cli/src/main/kotlin/net/voxelpi/varp/cli/command/commands/TagsCommand.kt
@@ -4,13 +4,14 @@ import kotlinx.coroutines.runBlocking
 import net.voxelpi.event.annotation.Subscribe
 import net.voxelpi.varp.cli.command.CommandsRegistrationEvent
 import net.voxelpi.varp.cli.command.parser.tree.nodeParser
-import net.voxelpi.varp.warp.Folder
 import net.voxelpi.varp.warp.Node
-import net.voxelpi.varp.warp.Root
+import net.voxelpi.varp.warp.NodeParent
 import net.voxelpi.varp.warp.Warp
 import org.incendo.cloud.description.Description
 import org.incendo.cloud.kotlin.extension.buildAndRegister
+import org.incendo.cloud.kotlin.extension.suggestionProvider
 import org.incendo.cloud.parser.standard.StringParser.quotedStringParser
+import org.incendo.cloud.suggestion.SuggestionProvider
 
 object TagsCommand {
 
@@ -19,8 +20,45 @@ object TagsCommand {
         val cli = event.cli
         val commandManager = event.commandManager
 
-        commandManager.buildAndRegister("tag", Description.description("Tags a node")) {
+        commandManager.buildAndRegister("tags", Description.description("Lists the tags of a node"), arrayOf("tag")) {
             required("node", nodeParser { cli.tree })
+
+            handler { context ->
+                val node: Node = context["node"]
+
+                val tagList = node.tags.joinToString(", ") { "\"$it\"" }
+                context.sender().sendMessage("The node ${node.path} has the following ${node.tags.size} tags: $tagList")
+            }
+
+            registerCopy("list") {}
+        }
+
+        commandManager.buildAndRegister("tags", Description.description("Checks for a tag on a node"), arrayOf("tag")) {
+            required("node", nodeParser { cli.tree })
+            literal("has")
+            required("tag", quotedStringParser()) {
+                suggestionProvider = SuggestionProvider.blockingStrings { context, input ->
+                    context.get<Node>("node").tags.map {
+                        if (it.contains(' ')) "\"$it\"" else it
+                    }
+                }
+            }
+
+            handler { context ->
+                val node: Node = context["node"]
+                val tag: String = context["tag"]
+
+                if (tag in node.tags) {
+                    context.sender().sendMessage("The node ${node.path} has the tag \"${tag}\"")
+                } else {
+                    context.sender().sendMessage("The node ${node.path} does not have a tag \"${tag}\"")
+                }
+            }
+        }
+
+        commandManager.buildAndRegister("tags", Description.description("Adds a tag to a node"), arrayOf("tag")) {
+            required("node", nodeParser { cli.tree })
+            literal("add")
             required("tag", quotedStringParser())
 
             handler { context ->
@@ -29,27 +67,50 @@ object TagsCommand {
 
                 runBlocking {
                     when (node) {
-                        is Folder -> node.modify {
-                            tags += tag
-                        }
                         is Warp -> node.modify {
                             tags += tag
                         }
-                        is Root -> node.modify {
+                        is NodeParent -> node.modify {
                             tags += tag
                         }
                     }
                 }
+                context.sender().sendMessage("Added the tag \"$tag\" to the node ${node.path}")
             }
         }
 
-        commandManager.buildAndRegister("tags", Description.description("Tags a node")) {
+        commandManager.buildAndRegister("tags", Description.description("Removes a tag from a node"), arrayOf("tag")) {
             required("node", nodeParser { cli.tree })
+            literal("remove")
+            required("tag", quotedStringParser()) {
+                suggestionProvider = SuggestionProvider.blockingStrings { context, input ->
+                    context.get<Node>("node").tags.map {
+                        if (it.contains(' ')) "\"$it\"" else it
+                    }
+                }
+            }
 
             handler { context ->
                 val node: Node = context["node"]
+                val tag: String = context["tag"]
 
-                context.sender().sendMessage("The node \"${node.path}\" has the following ${node.state.tags.size} tags: ${node.state.tags.map { "\"$it\"" }.joinToString(", ")}")
+                // Check if the node has the given tag.
+                if (tag !in node.tags) {
+                    context.sender().sendMessage("The node ${node.path} does not have a tag \"${tag}\"")
+                    return@handler
+                }
+
+                runBlocking {
+                    when (node) {
+                        is Warp -> node.modify {
+                            tags -= tag
+                        }
+                        is NodeParent -> node.modify {
+                            tags -= tag
+                        }
+                    }
+                }
+                context.sender().sendMessage("Remove the tag \"$tag\" to the node ${node.path}")
             }
         }
     }
