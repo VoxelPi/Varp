819fe94baa6def136b765854204d055bffc05408
Peter Smek <smek.peter@gmail.com>
2024-11-24 16:33:41 +0100

    Move mounts per default

--- varp-cli/src/main/kotlin/net/voxelpi/varp/cli/command/commands/MoveCommand.kt
+++ varp-cli/src/main/kotlin/net/voxelpi/varp/cli/command/commands/MoveCommand.kt
@@ -5,6 +5,8 @@ import net.voxelpi.event.annotation.Subscribe
 import net.voxelpi.varp.cli.command.CommandsRegistrationEvent
 import net.voxelpi.varp.cli.command.parser.path.nodeParentPathParser
 import net.voxelpi.varp.cli.command.parser.tree.nodeChildParser
+import net.voxelpi.varp.option.MoveMountsOptions
+import net.voxelpi.varp.option.assign
 import net.voxelpi.varp.warp.NodeChild
 import net.voxelpi.varp.warp.path.NodeParentPath
 import org.incendo.cloud.description.Description
@@ -24,13 +26,16 @@ object MoveCommand {
             required("destination", nodeParentPathParser { cli.tree })
             optional("id", stringParser())
 
+//            flag("direct", aliases = arrayOf("direct"))
+
             handler { context ->
                 val node: NodeChild = context["node"]
                 val destination: NodeParentPath = context["destination"]
                 val id = context.optional<String>("id").getOrNull()
+                val moveMounts = !context.flags().isPresent("direct")
 
                 runBlocking {
-                    node.move(destination, id).getOrThrow()
+                    node.move(destination, id, listOf(MoveMountsOptions assign moveMounts)).getOrThrow()
                 }
             }
         }
